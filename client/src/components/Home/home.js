585a57c04f44aaa0b75ec56c2d3b497e1f45385f
sophie homer <sophiehomer94@gmail.com>
2022-06-17 13:31:59 -0700

    styles

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,70 +1,75 @@
-import Auth from '../../utils/auth';
-import './home.css'
-import { useQuery } from '@apollo/client';
-import { QUERY_ME_BASIC, GET_USER_POSTS } from '../../utils/queries';
-import { Link } from 'react-router-dom';
-import Login from '../Login/login';
-import Header from '../Header/header.js';
-import { AiOutlineLike } from 'react-icons/ai'
-import { AiOutlineDislike } from 'react-icons/ai'
-
+import Auth from "../../utils/auth";
+import "./home.css";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME_BASIC, GET_USER_POSTS } from "../../utils/queries";
+import { Link } from "react-router-dom";
+import Login from "../Login/login";
+import Header from "../Header/header.js";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
 
 // import { AiOutlineDown } from 'react-icons/ai';
-import Accordion from 'react-bootstrap/Accordion';
+// import Accordion from 'react-bootstrap/Accordion';
 
 function Home() {
-    const { data } = useQuery(QUERY_ME_BASIC);
-    const { data: postQuery } = useQuery(GET_USER_POSTS);
-    // user information
-    const username = data?.me.username || '';
-    // Post Info
-    const postData = postQuery?.posts || [];
+  const { data } = useQuery(QUERY_ME_BASIC);
+  const { data: postQuery } = useQuery(GET_USER_POSTS);
+  // user information
+  const username = data?.me.username || "";
+  // Post Info
+  const postData = postQuery?.posts || [];
 
-    // check if user is logged in
-    const loggedIn = Auth.loggedIn()
-    return (
+  // check if user is logged in
+  const loggedIn = Auth.loggedIn();
+  return (
+    <>
+      {loggedIn ? (
         <>
-            {loggedIn ?
-                <>
-                    <Header />
-                    <section id="loggedInView">
-                    <h2 className="welcomeText">Welcome, {username}!</h2>                        
-                        {postData.map((post, index) =>
-                        (  
-                            <section className="discussion-post" key={index}>
-
-                            <div className="accordionHeaderDiv"> 
-                                <h3 id="username-post">{post.username}</h3>
-                                <p className="postDateCreated">{post.createdAt}</p>
-                            </div>  
-                            <div>
-                                <h4 id="userTitle-post"><Link to={`/Single-post/${post._id}`}>{post.postTitle}</Link></h4>
-                            </div> 
-                                <p id="postText">{post.postText}</p>
-                                <div className="likesBanMeterDiv">
-                                <div id="likes-dislikes">
-                                    {post.likesLength}<a className='voteBtn'><AiOutlineLike /></a>
-                                    {post.dislikesLength}<a className='voteBtn'><AiOutlineDislike /></a>
-                                </div>
-                                {/* <p id="ban-meter-p">Ban Meter
+          <Header />
+          <section id="loggedInView">
+            <h1 className="welcomeText">Welcome, {username}!</h1>
+            {postData.map((post, index) => (
+              <section className="discussion-post" key={index}>
+                <div className="accordionHeaderDiv">
+                  <h2 id="username-post">{post.username}</h2>
+                  <p className="postDateCreated">{post.createdAt}</p>
+                </div>
+                <div>
+                  <h3 id="userTitle-post">
+                    <Link to={`/Single-post/${post._id}`}>
+                      {post.postTitle}
+                    </Link>
+                  </h3>
+                </div>
+                <p id="postText">{post.postText}</p>
+                <div className="likesBanMeterDiv">
+                  <div id="likes-dislikes">
+                    {post.likesLength}
+                    <a className="voteBtn">
+                      <AiOutlineLike />
+                    </a>
+                    {post.dislikesLength}
+                    <a className="voteBtn">
+                      <AiOutlineDislike />
+                    </a>
+                  </div>
+                  {/* <p id="ban-meter-p">Ban Meter
                                     <progress id="banMeter" value={post.banMeter} max="0.6">{post.banMeter}</progress>
                                 </p> */}
-                                </div>
-                            </section>
-                        ))}
-                    </section>
-                </>
-                :
-                <>
-                     <div className="loginContainer">
-                        <Login />
-                    </div>
-                </>
-
-
-            }
+                </div>
+              </section>
+            ))}
+          </section>
         </>
-    )
+      ) : (
+        <>
+          <div className="loginContainer">
+            <Login />
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 
 export default Home;
--- client/src/components/Profile/profile.js
+++ client/src/components/Profile/profile.js
@@ -1,87 +1,84 @@
-import Auth from '../../utils/auth';
-import { useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { QUERY_USER } from '../../utils/queries';
-import { useQuery, useMutation } from '@apollo/client';
-import { ADD_POST, DELETE_POST } from '../../utils/mutations';
-import Accordion from 'react-bootstrap/Accordion';
-import './profile.css'
-import Header from '../Header/header.js';
-import deleteSound from '../../assets/sounds/delete-sound.wav';
+import Auth from "../../utils/auth";
+import { useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { QUERY_USER } from "../../utils/queries";
+import { useQuery, useMutation } from "@apollo/client";
+import { ADD_POST, DELETE_POST } from "../../utils/mutations";
+import "./profile.css";
+import Header from "../Header/header.js";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+import { FaUserFriends } from "react-icons/fa";
 
-function Profile () {
+function Profile() {
+  // get ID and query a user's info
+  const { id: userId } = useParams();
+  const { data } = useQuery(QUERY_USER, {
+    variables: { id: userId },
+  });
 
-    // sound function
-    const deleteSoundNoise = new Audio(deleteSound);
-    deleteSoundNoise.loop = false;
-    deleteSoundNoise.volume = 0.5;
+  // Get username and friends
+  const userInfo = data?.user || [];
+  const userPosts = data?.user.posts || [];
+  const userFriends = data?.user.friends || [];
 
-    // get ID and query a user's info
-    const { id: userId } = useParams()
-    const { data } = useQuery(QUERY_USER, {
-        variables: { id: userId },
-      });
+  const [addPost, { error }] = useMutation(ADD_POST);
+  const [deletePost] = useMutation(DELETE_POST);
 
-    // Get username and friends
-    const userInfo = data?.user || [];
-    const userPosts = data?.user.posts || [];
-    const userFriends = data?.user.friends || [];
-
-    const [addPost, { error }] = useMutation(ADD_POST);
-    const [deletePost] = useMutation(DELETE_POST);
-    
-    const loggedIn = Auth.loggedIn();
-    return (
+  const loggedIn = Auth.loggedIn();
+  return (
+    <>
+      {loggedIn ? (
         <>
-        {loggedIn ?
-            <>  
-            <Header />
-              <h2 className='welcomeText'>{userInfo.username}'s Profile Page</h2>
-              <main className="profilePage">
-                <section className="postsSection">
-                     {userPosts.map((post, index) =>
-                        (   <Accordion key={index}>
-                            <Accordion.Item eventKey="0">
-                            <section className="discussion-post" key={index}>
-                            <Accordion.Header>
-                            <div className="accordionHeaderDiv">     
-                            <h4 id="userTitle-post"><Link to={`/Single-post/${post._id}`}>{post.postTitle}</Link></h4>
-                                <h2 id="username-post">{userInfo.username}</h2>
-                                <p>{post.createdAt}</p>
-                            </div>      
-                                </Accordion.Header>
-                                <Accordion.Body>
-                                <p id="postText">{post.postText}</p>
-                                <div id="likes-dislikes">
-                                    <p>{post.likesLength}<a>  👍</a></p>
-                                    <p>{post.dislikesLength}<a>  👎</a></p>
-                                </div>
-                                <p id="ban-meter-p">Ban Meter: </p>
-                                <progress id="banMeter" value={post.banMeter} max="0.6">{post.banMeter}</progress>
-                                </Accordion.Body>
-                            </section>
-                            </Accordion.Item>
-                            </Accordion>
-                        ))}
+          <Header />
+          <h1 className="welcomeText">{userInfo.username}</h1>
+          <main className="profilePage">
+            <section className="postsSection">
+              {userPosts.map((post, index) => (
+                <section className="discussion-post" key={index}>
+                  <div className="accordionHeaderDiv">
+                    <h2 id="username-post">{userInfo.username}</h2>
+                    <p className="postDateCreated">{post.createdAt}</p>
+                  </div>
+                  <div>
+                    <h4 id="userTitle-post">
+                      <Link to={`/Single-post/${post._id}`}>
+                        {post.postTitle}
+                      </Link>
+                    </h4>
+                  </div>
+                  <p id="postText">{post.postText}</p>
+                  <div id="likes-dislikes">
+                    {post.likesLength}
+                    <a className="voteBtn">
+                      <AiOutlineLike />
+                    </a>
+                    {post.dislikesLength}
+                    <a className="voteBtn">
+                      <AiOutlineDislike />
+                    </a>
+                  </div>
                 </section>
-                <section className="friendsSection">
-                    <section id="friendsSectionBorder">
-                    <h4 className="friendsText">Friends</h4>
-                    {userFriends.map((friend, index) => (
-                        <div key={index}>
-                        <Link to={`/profile/${friend._id}`}>{friend.username}</Link>
-                        </div>
-                    ))}
-                      </section>
-                </section>
-            </main> 
-            </>
-            :
-            <>
-                <p>You must be to logged in to proceed</p>
-            </>
-            }
+              ))}
+            </section>
+            <section className="friendsSection">
+              <section id="friendsSectionBorder">
+                <h4 className="friendsText">Friends<FaUserFriends/></h4>
+                {userFriends.map((friend, index) => (
+                  <div key={index}>
+                    <Link to={`/profile/${friend._id}`}>{friend.username}</Link>
+                  </div>
+                ))}
+              </section>
+            </section>
+          </main>
         </>
-    )
+      ) : (
+        <>
+          <p>You must be to logged in to proceed</p>
+        </>
+      )}
+    </>
+  );
 }
 export default Profile;
