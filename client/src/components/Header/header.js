86825effbc0beb169aa771fa153761c17f839a68
sophie homer <sophiehomer94@gmail.com>
2022-07-06 13:47:42 -0700

    changed logo to white

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -3,8 +3,7 @@ import './header.css'
 import Nav from '../Nav/nav.js'
 // import { Link } from 'react-router-dom'
 import Auth from '../../utils/auth'
-// import logo1 from '../../assets/images/lt_logo_caps.png'
-import logo from '../../assets/images/dk_logo_caps.png'
+import logo from '../../assets/images/lt_logo_caps.png'
 
 const Header = () => {
   return (
